be74c41289d9071dfe34df5184e66a1c6527e2fb
okue_hidetaka <okue@as-systems.jp>
2024-05-18 21:24:29 +0900

    firstCommit

---
+++ FileManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileManager
+//
+//  Created by 奥江英隆 on 2024/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
